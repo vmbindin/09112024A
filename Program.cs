5d54a26a963be27f8c20028caea5a5edb8f9dc14
Muhammad Burhan Bin Din (Centific Technologies inc) <v-mbindin@microsoft.com>
2024-11-09 15:42:34 +0800

    System.commadLine program

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.CommandLine;
+using System.Threading.Tasks;
+using System.IO;
+using System.Linq;
+
+
+namespace scl;
+
+class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        var fileOption = new Option<FileInfo?>(
+            name: "--file",
+            description: "The file to read and display on the console.");
+
+        var rootCommand = new RootCommand("Sample app for System.CommandLine");
+        rootCommand.AddOption(fileOption);
+
+        rootCommand.SetHandler((file) => 
+            { 
+                ReadFile(file!); 
+            },
+            fileOption);
+            
+        if (args.Length == 0) 
+        {
+            NoArgs();
+            return 0;
+        }
+        return await rootCommand.InvokeAsync(args);
+    }
+
+    static void ReadFile(FileInfo file)
+    {
+        File.ReadLines(file.FullName).ToList()
+            .ForEach(line => Console.WriteLine(line));
+    }
+
+    static void NoArgs()
+    {
+        Console.WriteLine("Program Created by Burhan");
+        Console.WriteLine("If no idea type ProgramO -h | ProgramO --help");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # 09112024A
 console project that utilizes System.CommandLine library
+
+## help
+
+`dotnet --help`
+
+## create
+
+`dotnet new console`
+
+## add package
+
+`dotnet add package System.CommandLine --prerelease`
+
+## build
+
+`dotnet build`
+
+## run
+
+`dotnet run -- --file <example file>`
+
+## publish
+
+`dotnet publish -o out`
+
+## run publish
+
+`out\ProgramO --file <filename>`
+`out\ProgramO --help`
+
+## references
+[web-commandline](https://learn.microsoft.com/en-us/dotnet/standard/commandline/)
+[git-commandline](https://github.com/dotnet/docs/blob/main/docs/standard/commandline/get-started-tutorial.md)
+[version](https://andrewlock.net/version-vs-versionsuffix-vs-packageversion-what-do-they-all-mean/#informationalversion)
